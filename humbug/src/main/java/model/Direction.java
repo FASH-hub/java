021033dba5b0dd1881d01cbea1e6c24535794851
Léon FASHINGABO <53854@etu.he2b.be>
2021-12-14 17:13:42 +0100

    [add] enum Direction

---
+++ humbug/src/main/java/model/Direction.java
@@ -0,0 +1,33 @@
+package model;
+
+/**
+ * Represents 4 cardinal points used to define the animals' direction while moving.
+ */
+public enum Direction {
+
+    NORTH (-1, 0), SOUTH(1, 0), EAST(0, 1), WEST(0, -1);
+
+    private int deltaRow;
+    private int deltaColumn;
+
+    Direction(int row, int column) {
+        this.deltaRow = row;
+        this.deltaColumn = column;
+    }
+
+    /**
+     * Simple row's getter
+     * @return row
+     */
+    public int getDeltaRow() {
+        return deltaRow;
+    }
+
+    /**
+     * Simple column's getter
+     * @return column
+     */
+    public int getDeltaColumn() {
+        return deltaColumn;
+    }
+}
