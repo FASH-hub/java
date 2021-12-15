ba127ee25eaa30244fb745b7fdbddf911981fb8d
Léon FASHINGABO <53854@etu.he2b.be>
2021-12-15 14:48:28 +0100

    [updates] javadoc

--- humbug/src/main/java/model/Direction.java
+++ humbug/src/main/java/model/Direction.java
@@ -2,10 +2,12 @@ package model;
 
 /**
  * Represents 4 cardinal points used to define the animals' direction while moving.
+ *
+ * @author Léon FASHINGABO <fashingaboleon@gmail.com>
  */
 public enum Direction {
 
-    NORTH (-1, 0), SOUTH(1, 0), EAST(0, 1), WEST(0, -1);
+    NORTH(-1, 0), SOUTH(1, 0), EAST(0, 1), WEST(0, -1);
 
     private int deltaRow;
     private int deltaColumn;
@@ -17,6 +19,7 @@ public enum Direction {
 
     /**
      * Simple row's getter
+     *
      * @return row
      */
     public int getDeltaRow() {
@@ -25,6 +28,7 @@ public enum Direction {
 
     /**
      * Simple column's getter
+     *
      * @return column
      */
     public int getDeltaColumn() {
--- humbug/src/main/java/model/SquareType.java
+++ humbug/src/main/java/model/SquareType.java
@@ -1,5 +1,11 @@
 package model;
 
+/**
+ * Represents the type of square on the board.
+ * Square is either GRASS or STAR
+ *
+ * @author Léon FASHINGABO <fashingaboleon@gmail.com>
+ */
 public enum SquareType {
     GRAS, STAR;
 }
