b05d003d0b81aa965a1a3eaf5003a19fd096c1ad
Léon FASHINGABO <53854@etu.he2b.be>
2022-02-02 23:36:52 +0100

    updates

--- customerApp/src/main/java/App.java
+++
@@ -1,8 +0,0 @@
-import static management.Data.customerList;
-
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println(" Client list : " + customerList() );
-    }
-}
