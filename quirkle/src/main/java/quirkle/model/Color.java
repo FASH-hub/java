5c8d67fcf981c99f79396c1ecaaee6e9b02fe91f
Léon FASHINGABO <53854@etu.he2b.be>
2023-09-19 14:18:40 +0200

    enumerations Color & Shape created

---
+++ quirkle/src/main/java/quirkle/model/Color.java
@@ -0,0 +1,8 @@
+package quirkle.model;
+
+/**
+ * Color represents color of tile.
+ */
+public enum Color {
+    BLUE, RED, GREEN, ORANGE, YELLOW, PURPLE;
+}
---
+++ quirkle/src/main/java/quirkle/model/Shape.java
@@ -0,0 +1,8 @@
+package quirkle.model;
+
+/**
+ * Shape represents the form of the tile.
+ */
+public enum Shape {
+    CROSS, SQUARE, ROUND, STAR, PLUS, DIAMOND;
+}
