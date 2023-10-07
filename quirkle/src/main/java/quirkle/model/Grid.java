4742e5b96c7ddc9cff0dc2bfcf4a7f10d892adbb
Léon FASHINGABO <53854@etu.he2b.be>
2023-10-07 14:46:35 +0200

    updates

--- quirkle/src/main/java/quirkle/model/Grid.java
+++ quirkle/src/main/java/quirkle/model/Grid.java
@@ -1,12 +1,14 @@
 package quirkle.model;
-import static quirkle.model.Bag.*;
+
+import static quirkle.utils.CodesUtils.*;
+
 
 /**
  * Represents the board of the game.
  */
 public class Grid {
 
-    private Tile[][] tiles;
+    private final Tile[][] tiles;
 
 
     public Grid() {
@@ -16,13 +18,25 @@ public class Grid {
 
     /**
      * Gives tile located at a specific position.
+     *
      * @return tile
      */
     public Tile getTile(Position pos) {
         return tiles[pos.row()][pos.column()];
     }
 
-    public void addFirst(Position position, Direction direction, Tile... tile) {
-
+    /**
+     * Puts a tile on the grid.
+     * The first player first specifies the number of tiles he has and proceeds by putting them on the grid on specified position and in specified direction
+     *
+     * @param position  specified direction in which tiles will be put
+     * @param direction specified position on which tiles are being put
+     * @param tile      tile to be put on the grid
+     */
+    public void addFirst(Position position, Direction direction, Tile tile) {
+        int nbTiles = readInt("How many tiles do you have? ");
+        for (int i = 0; i < nbTiles; i++) {
+            tiles[position.row() + direction.getDeltaRow()][position.column() + direction.getDeltaColumn()] = tile;
+        }
     }
 }
