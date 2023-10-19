bb60b0a02ce038c85ffc0703be7a2f332929e3ca
Fashingabo <leon.fashingabo@accenture.com>
2023-10-19 11:08:51 +0200

    updates

--- quirkle/src/main/java/quirkle/model/Grid.java
+++ quirkle/src/main/java/quirkle/model/Grid.java
@@ -26,6 +26,17 @@ public class Grid {
     }
 
     /**
+     * Checks if received position is on the board.
+     *
+     * @param position received position to be checked
+     * @return true
+     */
+    public boolean isInside(Position position) {
+        return position.row() >= 0 && position.row() < this.getSize()
+                && position.column() >= 0 && position.column() < this.getSize();
+    }
+
+    /**
      * Puts a tile on the grid.
      * The first player first specifies the number of tiles he has and proceeds by putting them on the grid on specified position and in specified direction
      *
@@ -33,10 +44,21 @@ public class Grid {
      * @param direction specified position on which tiles are being put
      * @param tile      tile to be put on the grid
      */
-    public void addFirst(Position position, Direction direction, Tile tile) {
-        int nbTiles = readInt("How many tiles do you have? ");
-        for (int i = 0; i < nbTiles; i++) {
-            tiles[position.row() + direction.getDeltaRow()][position.column() + direction.getDeltaColumn()] = tile;
+    public void putTile(Position position, Direction direction, Tile tile) {
+        if (isInside(position)) {
+            int nbTiles = readInt("How many tiles do you have? ");
+            for (int i = 0; i < nbTiles; i++) {
+                tiles[position.row() + direction.getDeltaRow()][position.column() + direction.getDeltaColumn()] = tile;
+            }
         }
+    }
+
+    /**
+     * Gives the number of grid's rows or columns.
+     *
+     * @return number of rows/columns
+     */
+    public int getSize() {
+        return this.tiles.length;
     }
 }
